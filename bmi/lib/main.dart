a3204401a354649be6fb6dc487e2a61dc7db9e05
DESKTOP-QUPDGLB\jjhw0 <jjhw0106@naver.com>
2022-08-28 17:22:06 +0900

    플러터_비만계산기 완성

--- bmi/lib/main.dart
+++ bmi/lib/main.dart
@@ -29,6 +29,16 @@ class BmiMain extends StatefulWidget {
 class _BmiMainState extends State<BmiMain> {
   final _formKey = GlobalKey<FormState>();
 
+  final _heightController = TextEditingController();
+  final _weightController = TextEditingController();
+
+  @override
+  void dispose() {
+    _heightController.dispose();
+    _weightController.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -47,6 +57,13 @@ class _BmiMainState extends State<BmiMain> {
                     border: OutlineInputBorder(),
                     hintText: '키',
                   ),
+                  controller: _heightController,
+                  validator: (value) {
+                    if(value!.trim().isEmpty){
+                      return "키를 입력하세요";
+                    }
+                    return null;  // validator의 반환값이 null이면 Error 없는 것
+                  },
                   keyboardType: TextInputType.number, // 숫자만 입력 가능
                 ),
                 const SizedBox(
@@ -57,6 +74,13 @@ class _BmiMainState extends State<BmiMain> {
                     border: OutlineInputBorder(),
                     hintText: "몸무게",
                   ),
+                  controller: _weightController,
+                  validator: (value) {
+                    if(value!.trim().isEmpty){
+                      return "몸무게를 입력하세요";
+                    }
+                    return null;  // validator의 반환값이 null이면 Error 없는 것
+                  },
                   keyboardType: TextInputType.number,
                 ),
                 Container(
@@ -64,8 +88,14 @@ class _BmiMainState extends State<BmiMain> {
                   alignment: Alignment.centerRight,
                   child: ElevatedButton(
                     onPressed:() {
+                      // 폼 입력값 검증
                       if (_formKey.currentState!.validate()) {
-
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context)=> BmiResult(
+                              double.parse(_heightController.text.trim()),
+                              double.parse(_weightController.text.trim()))
+                            )
+                        );
                       }
                     },
                     child: Text('결과'),
@@ -89,6 +119,10 @@ class BmiResult extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final bmi = weight / ((height/100)*(height/100));
+    print('bmi:$bmi');
+
     return MaterialApp(
       home:Scaffold(
         appBar: AppBar(
@@ -99,13 +133,53 @@ class BmiResult extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Text(
-                "정상",
+                _calcBmi(bmi),
                 style: TextStyle(fontSize: 36),
-              )
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              _buildIcon(bmi),
             ],
           )
         )
       ),
     );
+  }
+  String _calcBmi(double bmi){
+    var result= '저체중';
+    if(bmi >= 35){
+      result = '고도 비만';
+    } else if(bmi>=30){
+      result = '2단계 비만';
+    } else if(bmi>=25){
+      result = '1단계 비만';
+    } else if(bmi>=23){
+      result = '과체중';
+    } else if(bmi>=18.5){
+      result = '정상';
+    }
+    return result;
+  }
+  Widget _buildIcon(double bmi){
+    if(bmi>=23){
+      return Icon(
+        Icons.sentiment_very_dissatisfied,
+        color: Colors.red,
+        size:100,
+      );
+    } else if(bmi>=18.5){
+      return Icon(
+          Icons.sentiment_very_dissatisfied,
+          color: Colors.red,
+          size:100,
+      );
+    } else {
+      return Icon(
+        Icons.sentiment_dissatisfied,
+        color: Colors.orange,
+        size: 100,
+      );
+    }
   }
 }
