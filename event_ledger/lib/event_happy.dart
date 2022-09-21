dc17252fcd897601c4f067328d62cc6a35069630
정 호원 <jjhw0106@jinhaksa.net>
2022-09-21 16:43:43 +0900

    장부 어플리케이션 작성 중

--- event_ledger/lib/event_happy.dart
+++ event_ledger/lib/event_happy.dart
@@ -1,27 +1,19 @@
 import 'package:flutter/material.dart';
 
-// class happyMain extends StatefulWidget {
-//   const happyMain({Key? key}) : super(key: key);
-//
-//   @override
-//   State<happyMain> createState() => _happyMainState();
-// }
-//
-// class _happyMainState extends State<happyMain> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(x
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title:Text("Page 1"),
-//         ),
-//       )
-//     );
-//   }
-// }
-class happyMain extends StatelessWidget {
+class happyMain extends StatefulWidget {
   const happyMain({Key? key}) : super(key: key);
 
+  @override
+  State<happyMain> createState() => _happyMainState();
+}
+
+class _happyMainState extends State<happyMain> {
+
+  final _items = <HappyInfo>[];
+
+  var _nameController = TextEditingController();
+  var _dateController = TextEditingController();
+  var _moneyController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,34 +27,78 @@ class happyMain extends StatelessWidget {
           // mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(child:_buildTop()),
-            Container(child:_buildMiddle()),
+            _buildTop(),
+            Expanded(child:_buildMiddle()),
             Container(child:_buildBottom()),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home'
+        ),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home'
+        ),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home'
+        ),
+      ]),
+    );
+  }
+  Widget _buildTop(){
+    return _textFieldForm();
+  }
+  Widget _buildMiddle(){
+    return Container(
+        color: Colors.blue,
+        child:_listViewForm()
+    );
+  }
+  Widget _buildBottom(){
+    return Text("bottom");
+  }
+
+  void _addInfo(HappyInfo happyInfo){
+    setState(() {
+      _items.add(happyInfo);
+      _nameController.text = '';
+    });
+    print('nameController: $_nameController.ㅍ');
+  }
+  Widget _textFieldForm(){
+    return Container(
+      height: 100,
+      child: Row(
+        children: [
+          Flexible(flex:1,child:TextField(decoration: InputDecoration(border:OutlineInputBorder(),labelText: '이름')),),
+          SizedBox(width:5),
+          Flexible(flex:1,child:TextField(decoration: InputDecoration(border:OutlineInputBorder(),labelText: '날짜')),),
+          SizedBox(width:5),
+          Flexible(flex:1,child:TextField(decoration: InputDecoration(border:OutlineInputBorder(),labelText: '축의금')),),
+          SizedBox(width:5),
+          Flexible(flex:1,child:ElevatedButton(
+            child: Icon(Icons.add)
+            ,onPressed: ()=>_addInfo(
+              HappyInfo(
+              _nameController.text,
+              _dateController.text,
+              _moneyController.text
+              ),
+            ),
+          )
+          )
+        ],
       ),
     );
   }
 }
 
-Widget _buildTop(){
-  return SizedBox(
-    width: 200,
-    child: Row(
-      children: <Widget>[
-        _textFieldForm(),
-      ],
-    ),
-  );
-}
-Widget _buildMiddle(){
-  return Text('middle');
-}
-Widget _buildBottom(){
-  return Text('bottom');
-}
-
 class _MyCustomTextField extends StatefulWidget {
+
   const _MyCustomTextField({Key? key}) : super(key: key);
 
   @override
@@ -76,15 +112,22 @@ class _MyCustomTextFieldState extends State<_MyCustomTextField> {
   }
 }
 
-Widget _textFieldForm(){
-  return TextField(
-    keyboardType: TextInputType.number,
-    decoration: InputDecoration(
-      labelText: 'Type your Number',
-      hintText: '1234',
-    ),
 
-    onChanged: (value) {
-    },
+
+Widget _listViewForm(){
+  final items = <HappyInfo>[];
+  var _todoController = TextEditingController();
+  return ListView(
+    children: [],
   );
+}
+
+
+
+class HappyInfo{
+  String name;
+  String date;
+  String money;
+
+  HappyInfo(this.name, this.date, this.money);
 }
