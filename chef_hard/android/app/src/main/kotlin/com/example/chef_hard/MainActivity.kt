175f5b8c7eeba51483a2d04c4670e4733afe94a7
jjhw0106 <jjhw0106@naver.com>
2022-11-09 18:01:42 +0900

    코딩셰프 업데이트

---
+++ chef_hard/android/app/src/main/kotlin/com/example/chef_hard/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chef_hard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ chef_hard/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
