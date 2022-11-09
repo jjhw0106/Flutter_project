e1a8074d6468018e913da8379cce76ba5bc34456
jjhw0106 <jjhw0106@naver.com>
2022-11-09 12:18:50 +0900

    코드셰프 내용 업데이트

--- checf/lib/main.dart
+++ checf/lib/main.dart
@@ -1,3 +1,8 @@
+
+
+
+
+// 플러터 v2.0 버튼 정리
 // import 'package:flutter/material.dart';
 //
 // void main() => runApp(MyApp());
@@ -8,206 +13,251 @@
 //   @override
 //   Widget build(BuildContext context) {
 //     return MaterialApp(
-//       home: MyPage(),
+//       home: MyHome(),
 //     );
 //   }
 // }
 //
-// class MyPage extends StatelessWidget {
-//   const MyPage({Key? key}) : super(key: key);
+// class MyHome extends StatelessWidget {
+//   const MyHome({Key? key}) : super(key: key);
 //
 //   @override
 //   Widget build(BuildContext context) {
 //     return Scaffold(
 //       appBar: AppBar(
-//         title: Text('Snack Bar!'),
+//         title: Text('Button'),
 //         centerTitle: true,
 //       ),
 //       body: Center(
-//         child: ElevatedButton(
-//           child: Text('Click'),
-//           onPressed:() {
-//             ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-//                   content: Text('Clicked!!',
-//                   textAlign: TextAlign.center,
-//                   style: TextStyle(color: Colors.white),
-//                 ),
-//               backgroundColor: Colors.teal,
-//               duration: Duration(milliseconds: 1000),
-//
-//             )
-//             );
-//           },
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-
-// import 'package:flutter/material.dart';
-//
-// void main() => runApp(MyApp());
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Snack bar',
-//       theme: ThemeData(
-//         primarySwatch: Colors.red
-//       ),
-//       home: MyPage(),
-//     );
-//   }
-// }
-//
-// class MyPage extends StatelessWidget {
-//   const MyPage({Key? key}) : super(key: key);
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('SnackBar'),
-//         centerTitle: true,
-//       ),
-//       body: Center(
-//         child: ElevatedButton(
-//           child: Text(
-//             'Show me',
-//             style: TextStyle(color: Colors.white),
-//           ),
-//           onPressed: (){
-//             //SnackBar 구현
-//             ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-//                 content: Text('New Snack Bar'),
-//             )
-//             );
-//             // Scaffold.of(context).showSnackBar(snackbar)
-//
-//             print('button tapped!!!!!');
-//           },
-//         )
-//       ),
-//     );
-//   }
-// }
-
-
-// import 'package:flutter/material.dart';
-//
-// void main() => runApp(MyApp());
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//     debugShowCheckedModeBanner: false,
-//       title: 'BBANTO',
-//       home: Grade(),
-//     );
-//   }
-// }
-//
-// class Grade extends StatelessWidget {
-//   const Grade({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       drawer: Drawer(
-//         child: ListView(
-//           padding: EdgeInsets.zero,
-//           children: <Widget>[
-//             UserAccountsDrawerHeader(
-//               currentAccountPicture:
-//                 CircleAvatar(
-//                   backgroundImage: AssetImage("assets/1.png"),
-//                   backgroundColor: Colors.white,
-//                 ),
-//               otherAccountsPictures: <Widget>[
-//                 CircleAvatar(
-//                   backgroundImage: AssetImage('assets/1.png'),
-//                   backgroundColor: Colors.white,
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             TextButton(
+//               onPressed: (){},
+//               onLongPress: (){
+//                 print('Text Button');
+//               },
+//               child: Text('Text Button',
+//                 style: TextStyle(fontSize: 20),
+//               ),
+//               style: TextButton.styleFrom(
+//                 primary: Colors.red,
+//               ),
+//             ),
+//             ElevatedButton(
+//               onPressed: (){
+//                 print('Elevated Button');
+//               },
+//               child: Text('Elevated Button'),
+//               style: ElevatedButton.styleFrom(
+//                 primary: Colors.orangeAccent,
+//                 shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10),),
+//                 elevation: 0,
+//               ),
+//             ),
+//             ElevatedButton(
+//               onPressed: null, // onPressed에 null을 대입하면 비활성화 됨
+//               child: Text('비활성 버튼'),
+//               style: ElevatedButton.styleFrom(
+//                 primary: Colors.orangeAccent,
+//                 onSurface: Colors.pink,     // 비활성화 된 버튼의 색상 변경
+//               ),
+//             ),
+//             OutlinedButton(
+//               onPressed: (){
+//                 print('Outlined Button');
+//               },
+//               child: Text('Outlined Button'),
+//               style: OutlinedButton.styleFrom(
+//                 primary: Colors.green,
+//                 side: BorderSide(
+//                   color: Colors.green,
+//                   width: 2,
 //                 )
+//               ),
+//             ),
+//             // 아이콘 있는 버튼
+//             TextButton.icon(
+//               onPressed: (){print('Button with Icon');},
+//               icon: Icon(
+//                 Icons.home,
+//                 size: 30,
+//                 color: Colors.black87,
+//               ),
+//               label: Text('Go to Home'),
+//               style: TextButton.styleFrom(
+//                 primary: Colors.purple,     // 아이콘에서 색상 따로 지정안하면 아이콘에도 색상이 적용됨
+//               ),
+//             ),
+//             // 버튼들을 끝정렬 (가로방향, 공간없으면 세로로)
+//             ButtonBar(
+//               alignment: MainAxisAlignment.center,
+//               buttonPadding: EdgeInsets.all(20),
+//               children: [
+//                 TextButton(onPressed: (){}, child: Text('Text Button')),
+//                 ElevatedButton(onPressed: (){}, child: Text('Elevated Button')),
 //               ],
-//               accountName: Text('BBANTO'),
-//               accountEmail: Text('bbanto@bbanto.com'),
-//               onDetailsPressed: () {
-//                 print('arrow is clicked');
-//               },
-//               decoration: BoxDecoration(
-//                 color: Colors.red[200],
-//                 borderRadius: BorderRadius.only(
-//                   bottomLeft: Radius.circular(40.0),
-//                   bottomRight: Radius.circular(40.0),
-//                 ),
-//               ),
-//             ),
-//             ListTile(
-//               leading: Icon(Icons.home,
-//                 color: Colors.grey[850],
-//               ),
-//               title: Text('Home'),
-//               onTap: (){
-//                 print('tapped1!!!!!!!!!');
-//               },
-//               trailing: Icon(Icons.add),
-//             ),
-//             ListTile(
-//               leading: Icon(Icons.settings,
-//                 color: Colors.grey[850],
-//               ),
-//               title: Text('Setting'),
-//               onTap: (){
-//                 print('tapped2!!!!!!!!!');
-//               },
-//               trailing: Icon(Icons.add),
-//             ),
-//             ListTile(
-//               leading: Icon(Icons.question_answer,
-//                 color: Colors.grey[850],
-//               ),
-//               title: Text('Q&A'),
-//               onTap: (){
-//                 print('tapped3!!!!!!!!!');
-//               },
-//               trailing: Icon(Icons.add),
-//             ),
+//             )
 //           ],
-//         ),
-//       ),
+//         )
+//       )
+//     );
+//   }
+// }
+
+
+
+
+// Scaffold Messenger
+// import 'package:flutter/material.dart';
+//
+// void main() => runApp(MyApp());
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: MyPage(),
+//     );
+//   }
+// }
+//
+// class MyPage extends StatelessWidget {
+//   const MyPage({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
 //       appBar: AppBar(
-//         title: Text('Appbar icon menu'),
-//         centerTitle: true,
-//         elevation: 0.0,
-//         backgroundColor: Colors.amber[800],
-//         leading:
-//         IconButton(
-//           icon: Icon(Icons.menu),
-//           onPressed: () {
-//             print('menu button is clicked');
-//           },
-//         ),
-//         actions: <Widget>[
-//           IconButton(
-//             icon: Icon(Icons.shopping_cart),
-//             onPressed: (){
-//               print('Shopping cart button is clicked');
-//             },
-//           ),
-//           IconButton(
-//             icon: Icon(Icons.search),
-//             onPressed: (){
-//               print('Search button is clicked');
-//             },
-//           ),
-//         ],
+//         title: Text('Scaffold Messenger'),
 //       ),
+//       body: HomeBody(),
+//       floatingActionButton: FloatingActionButton(
+//         child: Icon(Icons.thumb_up),
+//         onPressed: (){
+//           ScaffoldMessenger.of(context).showSnackBar(
+//               SnackBar(
+//                 content: Text('Like a new SnackBar'),
+//                 duration: Duration(seconds: 5),
+//                 action: SnackBarAction(
+//                   label: 'Undo',
+//                   onPressed: (){
+//                     Navigator.push(
+//                       context,
+//                       MaterialPageRoute(builder: (context)=>ThirdPage()),
+//                     );
+//                   },
+//                 ),
+//               )
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+//
+// class HomeBody extends StatelessWidget {
+//   const HomeBody({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: ElevatedButton(
+//         child: Text('Go to the second Page'),
+//         onPressed: (){
+//           Navigator.push(context, MaterialPageRoute(builder: (context)=>SecondPage()));
+//         },
+//       ),
+//     );
+//   }
+// }
+//
+// class SecondPage extends StatelessWidget {
+//   const SecondPage({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Second Page'),
+//       ),
+//       body: Center(
+//         child: Text(
+//           '좋아요가 추가되었습니다.',
+//           style: TextStyle(
+//             fontSize: 20.0,
+//             color:Colors.redAccent,
+//           ),
+//         )
+//       )
+//     );
+//   }
+// }
+//
+// class ThirdPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return ScaffoldMessenger(
+//       child: Scaffold(
+//           appBar: AppBar(
+//             title: Text('Third Page'),
+//           ),
+//           body: Builder(
+//             builder: (context) {
+//               return Center(
+//                   child: Column(
+//                     mainAxisAlignment: MainAxisAlignment.center,
+//                     children: [
+//                       Text(
+//                         '좋아요를 취소하시겠습니까?',
+//                         style: TextStyle(
+//                           fontSize: 20.0,
+//                           color:Colors.redAccent,
+//                         ),
+//                       ),
+//                       ElevatedButton(
+//                           onPressed: (){
+//                             ScaffoldMessenger.of(context).showSnackBar(
+//                               SnackBar(
+//                                 content: Text('좋아요가 취소되었습니다'),
+//                                 duration: Duration(seconds: 3),
+//                               ),
+//                             );
+//                           },
+//                           child: Text('취소하기')
+//                       )
+//                     ],
+//                   )
+//               );
+//             }
+//           )
+//       ),
+//     );
+//   }
+// }
+
+
+
+// import 'package:checf/ScreenB.dart';
+// import 'package:checf/ScreenC.dart';
+// import 'package:flutter/material.dart';
+// import 'package:checf/ScreenA.dart';
+//
+// void main() => runApp(MyApp());
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       initialRoute: '/',
+//       routes: {
+//         '/'  : (context) => ScreenA(),
+//         '/b' : (context) => ScreenB(),
+//         '/c' : (context) => ScreenC(),
+//       },
 //     );
 //   }
 // }
