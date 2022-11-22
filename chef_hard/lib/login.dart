797d5e26d1171fd446466aadc01710e933fd5beb
jjhw0106 <jjhw0106@naver.com>
2022-11-22 18:11:23 +0900

    코딩셰프 강의 업데이트

--- chef_hard/lib/login.dart
+++ chef_hard/lib/login.dart
@@ -14,6 +14,7 @@ class LogIn extends StatefulWidget {
 class _LogInState extends State<LogIn> {
   TextEditingController controller = TextEditingController();
   TextEditingController controller2 = TextEditingController();
+  // String msg = "";
 
   @override
   Widget build(BuildContext context) {
@@ -33,75 +34,110 @@ class _LogInState extends State<LogIn> {
           )
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Padding(padding: const EdgeInsets.only(top: 50),),
-              Center(
-                child: Image(
-                  image: AssetImage('image/chef.gif'),
-                  width: 170,
-                  height: 190,
+      body: GestureDetector(
+        onTap: (){
+          FocusScope.of(context).unfocus();
+        },
+          // FocusManager.instance.primaryFocus?.unfocus(),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(padding: const EdgeInsets.only(top: 50),),
+                Center(
+                  child: Image(
+                    image: AssetImage('image/chef.gif'),
+                    width: 170,
+                    height: 190,
+                  ),
                 ),
-              ),
-            Form(child: Theme(
-                data: ThemeData(
-                  primaryColor: Colors.teal,
-                  inputDecorationTheme: InputDecorationTheme(
-                    labelStyle: TextStyle(
-                      color: Colors.teal,
-                      fontSize: 15.0,
+              Form(child: Theme(
+                  data: ThemeData(
+                    primaryColor: Colors.teal,
+                    inputDecorationTheme: InputDecorationTheme(
+                      labelStyle: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 15.0,
+                      )
                     )
-                  )
-                ),
-                child: Container(
-                  padding: EdgeInsets.all(40),
-                  child: Column(
-                    children: <Widget> [
-                      TextField(
-                        controller: controller,
-                        decoration: InputDecoration(
-                          labelText: 'Enter "Dice"',
+                  ),
+                  child: Container(
+                    padding: EdgeInsets.all(40),
+                    child: Column(
+                      children: <Widget> [
+                        TextField(
+                          controller: controller,
+                          decoration: InputDecoration(
+                            labelText: 'Enter "Dice"',
+                          ),
+                          keyboardType: TextInputType.emailAddress,
                         ),
-                        keyboardType: TextInputType.emailAddress,
-                      ),
-                      TextField(
-                        controller: controller2,
-                        decoration: InputDecoration(
-                          labelText: 'Enter "Password"',
+                        TextField(
+                          controller: controller2,
+                          decoration: InputDecoration(
+                            labelText: 'Enter "Password"',
+                          ),
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
                         ),
-                        keyboardType: TextInputType.text,
-                        obscureText: true,
-                      ),
-                      SizedBox(height: 35,),
-                      ButtonTheme(
-                        minWidth: 100,
-                        height: 50,
-                        child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              primary: Colors.orangeAccent,
-                              fixedSize: Size(35, 35),
-                            ),
-                            child: Icon(Icons.arrow_forward),
-                            onPressed: (){
-                              if('Dice' != controller.text){
-                                Exception("아이디가 틀렸습니다.");
-                              } else if('1234' != controller2.text){
-                                Exception("비밀번호가 틀렸습니다.");
-                              }
-                              Navigator.push(context,
-                                  MaterialPageRoute(builder: (context) => Dice(id: controller.text))
-                              );
-                            },
-                        )
-                      ),
-                    ],
-                  )
-                ))
-            )
-          ],
+                        SizedBox(height: 35,),
+                        ButtonTheme(
+                          minWidth: 100,
+                          height: 50,
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.orangeAccent,
+                                fixedSize: Size(35, 35),
+                              ),
+                              child: Icon(Icons.arrow_forward),
+                              onPressed: (){
+                                try{
+                                  if('Dice' != controller.text || 'Password' != controller2.text){
+                                    throw Exception("아이디 혹은 비밀번호가 틀렸습니다");
+                                  }
+                                  Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) => Dice(id: controller.text))
+                                  );
+                                } catch(msg){
+                                  _showAlert(msg.toString());
+                                }
+                                // if('Dice' != controller.text){
+                                //   Exception("아이디가 틀렸습니다.");
+                                // } else if('1234' != controller2.text){
+                                //   Exception("비밀번호가 틀렸습니다.");
+                                // }
+                              },
+                          )
+                        ),
+                      ],
+                    )
+                  ))
+              )
+            ],
+          ),
         ),
       ),
     );
   }
+  Future<void> _showAlert(String msg) async{
+    String message = msg;
+    print("Ffff"+ msg);
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context){
+        return AlertDialog(
+          title: const Text("로그인 에러"),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text("로그인 실패"),
+                Text(message),
+              ]
+            )
+          )
+        );
+      }
+    );
+  }
 }
+
