88917ca49db1e76e507afa118bd14320cb750da5
jjhw0106 <jjhw0106@naver.com>
2022-11-25 17:05:10 +0900

    codingchef update

--- chef_hard/lib/dice.dart
+++ chef_hard/lib/dice.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'dart:math';
+import 'package:fluttertoast/fluttertoast.dart';
+
 import 'package:flutter/material.dart';
 
 class Dice extends StatefulWidget {
@@ -9,42 +13,70 @@ class Dice extends StatefulWidget {
 }
 
 class _DiceState extends State<Dice> {
+  int leftDice = 1;
+  int rightDice = 2;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.orangeAccent,
-      appBar: AppBar(
-        backgroundColor: Colors.redAccent,
-        title: Text('DiceGame'),
-      ),
-      body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(32),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Expanded(
-                      flex: 1,
-                        child: Image.asset('image/dice1.png')
-                    ),
+    return Builder(
+      builder: (context) {
+        return Scaffold(
+          backgroundColor: Colors.orangeAccent,
+          appBar: AppBar(
+            backgroundColor: Colors.redAccent,
+            title: Text('DiceGame'),
+          ),
+          body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(32),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Expanded(
+                          flex: 1,
+                            child: Image.asset('image/dice$leftDice.png')
+                        ),
 
-                    SizedBox(width: 20),
+                        SizedBox(width: 20),
 
-                    Expanded(
-                      flex: 1,
-                        child: Image.asset('image/dice2.png')
+                        Expanded(
+                          flex: 1,
+                            child: Image.asset('image/dice$rightDice.png')
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ),
-              ElevatedButton(onPressed: (){}, child: )
-            ],
-          )
-      ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(minimumSize: Size(20, 40)),
+                    onPressed: (){
+                      setState(() {
+                        leftDice = getRandomNum();
+                        rightDice = getRandomNum();
+                      });
+                      showToast("토스트메시지");
+                    },
+                    child: Icon(Icons.play_arrow),
+                  )
+                ],
+              )
+          ),
+        );
+      }
     );
   }
+    int getRandomNum(){
+      return Random().nextInt(6)+1;
+    }
+    
+    void showToast(String message){
+      Fluttertoast.showToast(
+        msg: message,
+        backgroundColor: Colors.black,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM
+      );
+    }
 }
