7620f6053ba7e1ee0a5f2716541bede6c64ed6d8
jjhw0106 <jjhw0106@naver.com>
2022-12-30 16:07:04 +0900

    시간표 업데이트

--- autoplanpractice/lib/ui/timetable.dart
+++ autoplanpractice/lib/ui/timetable.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class Timetable extends StatelessWidget {
 
   List week = ['월', '화', '수', '목', '금', '토', '일'];
-  var kColumnLength = 14;
+  var kColumnLength = 32;
   double kFirstColumnHeight = 20;
   double kBoxSize = 60;
 
@@ -12,34 +12,58 @@ class Timetable extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('Provider')),
       body: Container(
-        width: double.infinity,
-        height: double.infinity,
+        // width: double.infinity,
+        // height: double.infinity,
         color: Colors.green,
-        child: Row(
+        child: Column(
+          crossAxisAlignment : CrossAxisAlignment.center,
           children: [
-            dayCol(0),
-            dayCol(1),
-            dayCol(2),
+            Text('fff'),
+            timeBoard(),
           ],),
         ),
       );
   }
-  Widget dayCol(int index){
+
+  Widget timeBoard(){
     return Container(
-      color: Colors.red,
-      child: Column(
-        children: [
-          Text('${week[index]}'),
-          Row(
-            children: [
-              VerticalDivider(
-              width: 1,
-              thickness: 1,
-              color: Colors.black,
-            )],
-          )
-        ],
-      )
+      height: 400,
+      // width: 400,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all()
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+            Row(
+              children: List.generate(week.length, (index) => classesByDay())
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // 하루(요일) 시간표 생성
+  Widget classesByDay(){
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: List.generate(kColumnLength, (index) => oneSubject())
+    );
+  }
+
+  // 과목 한 칸
+  Widget oneSubject(){
+    return Container(
+      height: 20,
+      width: 40,
+      decoration: BoxDecoration(
+        border: Border.all(),
+        color: Colors.blue,
+      ),
     );
   }
 }
