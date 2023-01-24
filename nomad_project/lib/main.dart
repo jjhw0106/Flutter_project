0ffa0a18aa96a1d2fd8cbc29b857020d2858525e
DESKTOP-QUPDGLB\jjhw0 <jjhw0106@naver.com>
2023-01-24 18:03:59 +0900

    노마드 플러터

---
+++ nomad_project/lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            fontSize: 30,
+            color: Colors.red,
+          ),
+        ),
+      ),
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const myLargeTitle(),
+              Text("$counter"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class myLargeTitle extends StatefulWidget {
+  const myLargeTitle({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<myLargeTitle> createState() => _myLargeTitleState();
+}
+
+class _myLargeTitleState extends State<myLargeTitle> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+          fontSize: 30, color: Theme.of(context).textTheme.titleLarge!.color),
+    );
+  }
+}
