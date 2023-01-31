c9645adfb715caf6ef6eb4cc0b4dc0073ab52c77
jjhw0106 <jjhw0106@naver.com>
2023-01-31 11:51:31 +0900

    merge

--- getx_practice/lib/controller.dart
+++ getx_practice/lib/controller.dart
@@ -6,6 +6,12 @@ class Controller extends GetxController{
   final person = Person().obs;
 
   void updateInfo(){
+    Rx<Person> p = Person().obs;
+    Person per = p();
+
+    Rx<String> str = 'Bill'.obs;
+    String s = str();
+
     // state의 현재 value값을 가져옴
     person.update((val) {
       val?.age++;
