9ff850dec7a7542457e3ea5e4606a39093b917b3
jjhw0106 <jjhw0106@jinhak.com>
2023-02-21 18:11:36 +0900

    book_info_dialog, study_enrollment update

--- dev_test/lib/ui/workbook/book_info_dialog.dart
+++ dev_test/lib/ui/workbook/book_info_dialog.dart
@@ -1,46 +1,139 @@
-import 'package:dev_test/controller/workbook/study_enrollment_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-Future bookInfoDialog(BuildContext context, dynamic selectedBook) async{
-  StudyEnrollmentController controller = Provider.of<StudyEnrollmentController>(context, listen:false);
+Future bookInfoDialog(BuildContext context, dynamic selectedBook) async {
+  const double fontSize = 13.7;
   return showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20.0),
-        ),
-      actions: [
-        SizedBox(child: Center(
-          child: Container(
-            height: 137, 
-            width: 108,
-            decoration: BoxDecoration(border: Border.all(color: Colors.black12)),
-            child: Image.network(
-              selectedBook['thumbnail'],
-              errorBuilder: (context, error, stackTrace) => const Center(child: Text('no Image')),
-              ))),
-        ),
-        SizedBox(child: Align(alignment:Alignment.centerLeft, child: Text(selectedBook['title']))),
-        SizedBox(child: Align(alignment:Alignment.centerLeft, child: Text(selectedBook['publisher']))),
-        SizedBox(child: Align(alignment:Alignment.centerLeft, child: Text(selectedBook['datetime']))),
-        Row(
-          children: [
-            ElevatedButton(
-              child: const Center(child: Text("취소")),
-              onPressed: () {
-                Navigator.pop(context);
-              },
+    context: context,
+    builder: (context) => AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      content: ConstrainedBox(
+        constraints: const BoxConstraints(maxHeight: 400, maxWidth: 400),
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          // 썸네일
+          _thumbnailBox(selectedBook),
+          const SizedBox(height: 20),
+          // 제목
+          SizedBox(
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(selectedBook['title'],
+                  style: const TextStyle(
+                      fontSize: fontSize + 2,
+                      fontFamily: 'NotoSansKR-Medium',
+                      fontWeight: FontWeight.bold)),
             ),
-            ElevatedButton(
-              child: const Center(child: Text("확인")),
-              onPressed: () {
-                Navigator.pop(context);
-              },
+          ),
+          const SizedBox(height: 15),
+          // 출판사
+          _dialogContentsRow(fontSize, selectedBook, "publisher", "출판사명"),
+          // 출판일
+          _dialogContentsRow(fontSize, selectedBook, "datetime", "출판일"),
+        ],
+        ),
+      ),
+      actions: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Expanded(
+              child: _buttonStyleButton(context, "취소"),
+            ),
+            Expanded(
+              child: _buttonStyleButton(context, "선택"),
             ),
           ],
         ),
       ],
     ),
+  );
+}
+
+Widget _dialogContentsRow(double fontSize, selectedBook, searchingKey, searchingKeyName) {
+  return SizedBox(
+    child: Align(
+      alignment: Alignment.centerLeft,
+
+      child: Container(
+        height:40,
+        decoration: const BoxDecoration(border: Border(top: BorderSide(width: 1,color: Color(0xffeeeeee)))),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 60,
+              child: Text(
+                searchingKeyName,
+                style: TextStyle(
+                    color: const Color(0xff66000000),
+                    fontSize: fontSize,
+                    fontFamily: 'NotoSansKR-Medium',
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Text(
+              selectedBook[searchingKey],
+              style: TextStyle(
+                fontSize: fontSize,
+                fontFamily: 'NotoSansKR-Medium',
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+Widget _thumbnailBox(selectedBook) {
+  return SizedBox(
+    child: Center(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 20),
+        child: Container(
+          decoration: const BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                offset: Offset(0.0, 1.0), //(x,y)
+                blurRadius: 6.0,
+              )
+            ],
+          ),
+          height: 200,
+          child: Image.network(
+            selectedBook['thumbnail'],
+            fit: BoxFit.cover,
+            errorBuilder: (context, error, stackTrace) => const Center(child: Text('no Image')),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+Widget _buttonStyleButton(BuildContext context, String message) {
+  return ElevatedButton(
+    onPressed: () => Navigator.pop(context),
+    style: ButtonStyle(
+      backgroundColor: MaterialStateProperty.all<Color>(const Color(0xfff6f7fa)),
+      foregroundColor: MaterialStateProperty.resolveWith<Color>((states) {
+        if (states.contains(MaterialState.pressed)) {
+          return Colors.white;
+        }
+        return Colors.black;
+      }),
+      textStyle: MaterialStateProperty.all<TextStyle>(
+          const TextStyle(color: Colors.white)),
+      elevation: MaterialStateProperty.all(0),
+      overlayColor: MaterialStateProperty.resolveWith((states) {
+        return states.contains(MaterialState.pressed)
+            ? const Color(0xff5664cd)
+            : null;
+      }),
+    ),
+    child: Text(message),
   );
 }
--- dev_test/lib/ui/workbook/study_enrollment.dart
+++ dev_test/lib/ui/workbook/study_enrollment.dart
@@ -15,7 +15,7 @@ class _StudyEnrollmentState extends State<StudyEnrollment> {
   int _PREV = 1;
   int _NEXT = 2;
   final _textController = TextEditingController();
-  List<BookInfo> bookInfo=[];
+  List<BookInfo> bookInfo = [];
   late StudyEnrollmentController studyController;
   late dynamic bookList;
   // 최초 검색 전 표시 문구
@@ -24,7 +24,8 @@ class _StudyEnrollmentState extends State<StudyEnrollment> {
   @override
   void initState() {
     super.initState();
-    studyController = Provider.of<StudyEnrollmentController>(context, listen: false);
+    studyController =
+        Provider.of<StudyEnrollmentController>(context, listen: false);
     studyController.onInit(context);
   }
 
@@ -33,24 +34,24 @@ class _StudyEnrollmentState extends State<StudyEnrollment> {
     studyController = context.watch<StudyEnrollmentController>();
     return Scaffold(
       appBar: AppBar(
-          iconTheme: const IconThemeData(
-            color: Colors.black, //change your color here
-            ),
-          elevation: 0,
-          backgroundColor: Colors.blue,
-          automaticallyImplyLeading: true,
-          centerTitle: true,
-          title: const Text("문제집 등록", style: TextStyle(color: Colors.black,fontSize: 21.7)),
+        iconTheme: const IconThemeData(
+          color: Colors.black, //change your color here
         ),
-      
-      body: !studyController.isLoading ? 
-      GestureDetector(
-          onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-          child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: TextField(
+        elevation: 0,
+        backgroundColor: Colors.blue,
+        automaticallyImplyLeading: true,
+        centerTitle: true,
+        title: const Text("문제집 등록",
+            style: TextStyle(color: Colors.black, fontSize: 21.7)),
+      ),
+      body: !studyController.isLoading
+          ? GestureDetector(
+              onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: TextField(
                       controller: _textController,
                       onSubmitted: (value) {
                         _searchBooks();
@@ -59,34 +60,36 @@ class _StudyEnrollmentState extends State<StudyEnrollment> {
                       decoration: InputDecoration(
                         hintText: "찾으실 교재명을 적어주세요",
                         isDense: true,
-                        prefixIcon: const Icon(Icons.search,size: 30),
+                        prefixIcon: const Icon(Icons.search, size: 30),
                         suffix: Padding(
-                          padding: const EdgeInsets.only(right: 20 ),
+                          padding: const EdgeInsets.only(right: 20),
                           child: ElevatedButton(
                             onPressed: () {
                               _searchBooks();
                               _textController.clear();
-                              },
+                            },
                             style: ElevatedButton.styleFrom(
                               backgroundColor: Colors.white,
                               elevation: 0,
-                            ), 
-                            child: const Text("검색",style: TextStyle(color: Colors.black),),
+                            ),
+                            child: const Text("검색", style: TextStyle(color: Colors.black),
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
                   Expanded(
-                    child: studyController.searchTextList.isEmpty ? 
-                  Center(child: Text(screenMessage)) : 
-                    ListView.builder(
-                      itemCount: studyController.searchTextList.length,
-                      itemBuilder: (context, index) {
-                        return bookItemContainer(context, studyController.searchTextList[index]);
-                      },
-                    ),
+                    child: studyController.searchTextList.isEmpty
+                      ? Center(child: Text(screenMessage))
+                      : ListView.builder(
+                          itemCount: studyController.searchTextList.length,
+                          itemBuilder: (context, index) {
+                            return bookItemContainer(context, studyController.searchTextList[index]);
+                          },
+                        ),
                   ),
+                  Container(height: 125, color: Colors.white, child: const Text("원하는 문제집이 없으시다면 \n검색어를 상세하게 입력 후 재검색해 주세요!"),),
                   Padding(
                     padding: const EdgeInsets.fromLTRB(0, 25, 0, 25),
                     child: Row(
@@ -94,50 +97,52 @@ class _StudyEnrollmentState extends State<StudyEnrollment> {
                       children: [
                         moveStep(context, _PREV, "뒤로가기"),
                         moveStep(context, _NEXT, "다음단계")
-                      ]
-                    ),  
+                      ]),
                   )
                 ],
               ),
-          ) 
+            )
           : const Center(
               child: CircularProgressIndicator(
-                valueColor: AlwaysStoppedAnimation<Color>(Color.fromARGB(255, 175, 45, 45)),
+                valueColor: AlwaysStoppedAnimation<Color>(
+                    Color.fromARGB(255, 175, 45, 45)),
                 strokeWidth: 10.0,
               ),
             ),
     );
   }
+
   // 검색결과 x 문구세팅 및 책검색 API 검색결과 세팅
-  void _searchBooks(){
+  void _searchBooks() {
     studyController.setBookInfoList(_textController.text);
     screenMessage = studyController.resultYn();
   }
 }
 
-Widget moveStep(BuildContext context, int move, String message,{String? next}){
-  ButtonStyle style =  ElevatedButton.styleFrom(
+Widget moveStep(BuildContext context, int move, String message,
+    {String? next}) {
+  ButtonStyle style = ElevatedButton.styleFrom(
     textStyle: const TextStyle(fontSize: 17.7, color: Colors.black),
     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80)),
-    minimumSize: const Size(143.3,54),
-    );
+    minimumSize: const Size(143.3, 54),
+  );
   return ElevatedButton(
-    onPressed: (){
-      if(move == 1){
+    onPressed: () {
+      if (move == 1) {
         Navigator.pop(context);
-      }else if(move == 2){
-        
-      }
+      } else if (move == 2) {}
     },
-    style: style, 
+    style: style,
     child: Text(message),
   );
 }
 
 // 검색된 도서 표시 레이아웃
-Widget bookItemContainer(BuildContext context, dynamic selectedBook){
-  return SizedBox(
-    height: 70,
+Widget bookItemContainer(BuildContext context, dynamic selectedBook) {
+
+  return Container(
+    decoration: const BoxDecoration(border: Border(top: BorderSide(width: 1, color: Color(0xffeeeeee)))),
+    height: 60,
     width: 100,
     child: Padding(
       padding: const EdgeInsets.fromLTRB(27.3, 0, 17.3, 0),
@@ -147,23 +152,24 @@ Widget bookItemContainer(BuildContext context, dynamic selectedBook){
           Flexible(
             flex: 8,
             child: Text(
-              selectedBook['title'], 
+              selectedBook['title'],
               overflow: TextOverflow.ellipsis,
               style: const TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 16,
-                fontFamily: 'Noto_Sans_KR-Medium'),
-                ),
+                fontFamily: 'Noto_Sans_KR-Medium'
+              ),
+            ),
           ),
           Flexible(
             flex: 2,
             child: IconButton(
-              icon: const Icon(Icons.arrow_forward_ios_rounded), 
+              icon: const Icon(Icons.arrow_forward_ios_rounded),
               onPressed: () {
                 bookInfoDialog(context, selectedBook);
-              }
-            )
-          )
+              },
+            ),
+          ),
         ],
       ),
     ),
