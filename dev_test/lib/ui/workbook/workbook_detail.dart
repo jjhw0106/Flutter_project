0d634f6e331c2d3bc2ca80442719977a96f54336
정호원 <jjhw0106@naver.com>
2023-03-13 21:17:42 +0900

    TextField 문제 해결

--- dev_test/lib/ui/workbook/workbook_detail.dart
+++ dev_test/lib/ui/workbook/workbook_detail.dart
@@ -7,7 +7,7 @@ import 'package:flutter/src/widgets/framework.dart';
 class WorkbookDetail extends StatefulWidget {
   late final KakaoWorkbook kakaoWorkbook;
 
-  WorkbookDetail(selectedBook, {super.key}){
+  WorkbookDetail(selectedBook, {super.key}) {
     kakaoWorkbook = selectedBook;
   }
 
@@ -16,90 +16,68 @@ class WorkbookDetail extends StatefulWidget {
 }
 
 class _WorkbookDetailState extends State<WorkbookDetail> {
-  @override
+  // @override
   // List<TextEditingController> textEditingControllerList =[];
   final _textEditingController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBar(message: "문제집 등록"),
-      body: Column(
-        children: [
-          const Text("문제집명 입력"),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              const Text('교재명'),
-              Text(widget.kakaoWorkbook.title),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              const Text('학습 범위'),
-              TextField(
-                controller: _textEditingController,
-                onSubmitted: (value) => Text(value),
-                decoration: InputDecoration(
-                        hintText: "찾으실 교재명을 적어주세요",
-                        isDense: true,
-                        enabledBorder: const UnderlineInputBorder(
-                            borderSide: BorderSide(color: Color(0xff5664cd))),
-                        focusedBorder: const UnderlineInputBorder(
-                            borderSide: BorderSide(color: Color(0xff5664cd))),
-                        // border: OutlineInputBorder(borderSide: Border(bottom: BorderSide)),
-                        prefixIcon: const Icon(
-                          Icons.search,
-                          size: 30,
-                          color: Color(0xff5664cd),
-                        ),
-                        suffix: Padding(
-                          padding: const EdgeInsets.only(right: 20),
-                          child: ElevatedButton(
-                            onPressed: () {
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.transparent,
-                              elevation: 0,
-                            ),
-                            child: const Text(
-                              "검색",
-                              style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                        ),
-                      ), 
-                // const InputDecoration(
-                //   hintText: "시작분량",
-                //   isDense: true,
-                //   ),
-              ),
-            ],
-          ),
-
-          const Text("목표 달성 완료 일자 설정"),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              const Text('시작 일자'),
-              Text(widget.kakaoWorkbook.title),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              const Text('총학습기간'),
-              Text(widget.kakaoWorkbook.title),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              const Text('예상종료일자'),
-              Text(widget.kakaoWorkbook.title),
-            ],
-          ),
-        ],
-      ) 
-    );
+        appBar: appBar(message: "문제집 등록"),
+        body: Column(
+          children: [
+            const Text("문제집명 입력"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Text('교재명'),
+                Text(widget.kakaoWorkbook.title),
+              ],
+            ),
+            Row(
+              // mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Text('학습 범위'),
+                Expanded(
+                  child: TextField(
+                      controller: _textEditingController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: '학습 범위',
+                      )),
+                ),
+                Expanded(
+                  child: TextField(
+                      controller: _textEditingController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: '학습 범위',
+                      )),
+                ),
+              ],
+            ),
+            const Text("목표 달성 완료 일자 설정"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Text('시작 일자'),
+                Text(widget.kakaoWorkbook.title),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Text('총학습기간'),
+                Text(widget.kakaoWorkbook.title),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Text('예상종료일자'),
+                Text(widget.kakaoWorkbook.title),
+              ],
+            ),
+          ],
+        ));
   }
 }
